a8d7cc9ae986f148588250873dd24e79fc2eeeee
arvindvikram <arvindvikramms@gmail.com>
2026-04-13 16:54:30 +0530

    factorial program implemented

---
+++ Task1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Enter the number");
+        string num = Console.ReadLine();
+
+        if (!int.TryParse(num, out int number) || number < 0)
+        {
+            return;
+        }
+
+        long fact = factorial(number);
+        Console.WriteLine(fact);
+
+        Console.WriteLine("the factorial of number " + number + " is " + fact);
+    }
+
+    public static long factorial(int num)
+    {
+        if (num == 0 || num == 1)
+        {
+            return 1;
+        }
+        return num * factorial(num - 1);
+    }
+}
